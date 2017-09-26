fe5117160883830272499569ad5000533bef4518
Chris Davies <cdavies@gmail.com>
2017-09-26 17:42:30 -0400

    Adding images and jquery

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
+$(document).ready(function () {
     //your jquery method blocks here 
+    showAlbums();
+    imageChange();
 });
+//Hides albums on load
+$(function hideAlbums() {
+    $('.hidden').hide();
+});
+
+function showAlbums() {
+
+    $("#morebutton").click(function () {
+        $('.hidden').show();
+        $('#morebutton').hide();
+    });
+}
+function imageChange(){
+    $('.mainimage').click(function() {
+        $('.mainimage').animate({
+            height: '+=10%',
+            width: '+=10%'
+        });
+    });
+}
+
+
+/*$("button").click(function(){
+    $("div").animate({
+        left: '250px',
+        height: '+=150px',
+        width: '+=150px'
+    });
+})*/
