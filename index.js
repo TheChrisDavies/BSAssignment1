50d29174e3b1b58bfa58317f855b4647634c19f7
Chris Davies <cdavies@gmail.com>
2017-10-01 13:34:15 -0400

    updated styling, media qs and cleaning up code

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 // A $( document ).ready() block.
 $(document).ready(function () {
-    //your jquery method blocks here 
-    $(function hideSpotify() {
-        $('.spotify').hide();
-    });
-    $(function showSpotify() {
-        $('.btn-spotify').click(function () {
-            $('.spotify').toggle('slow');
-        })
-    });
+    //Comment section
     $(function appendComment() {
         $('.btn-comment').click(function () {
             var commentName = $("#usr").val();
@@ -16,6 +8,18 @@ $(document).ready(function () {
             $('#thecomment').append('<div class="bg-white col-xs-12 col-sm-12 col-md-12 col-md-offset-0 col-lg-12 col-lg-offset-0">' + commentName + ' wrote: <br/> ' + commentText + '</div>');
         })
     });
+    //Extra Function 1 - Hides all spotify iframes on load
+    $(function hideSpotify() {
+        $('.spotify').hide();
+    });
+    //Extra Function 2 - Loads all spotify iframes on button click
+    $(function showSpotify() {
+        $('.btn-spotify').click(function () {
+            $('.spotify').toggle('slow');
+        })
+    });
+    
+    //Main Image Hover - Main image changes image slightly when hovered over
     $(".mainimage").hover(function () {
         console.log('hovered');
         $('.mainimage').animate({
@@ -28,35 +32,3 @@ $(document).ready(function () {
         }, 'slow');
     });
 });
-
-// // Hide spotify on load
-// $(function hideSpotify() {
-//     $('.spotify').hide();
-// });
-// //Toggle Spotify playlists on button click
-// $(function showSpotify() {
-//     $('.btn-spotify').click(function () {
-//         $('.spotify').toggle('slow');
-//     })
-// });
-// // JQuery Comment functionality
-// $(function appendComment() {
-//     $('.btn-comment').click(function () {
-//         var commentName = $("#usr").val();
-//         var commentText = $("#comment").val();
-//         $('#thecomment').append('<div class="bg-white col-xs-12 col-sm-12 col-md-12 col-md-offset-0 col-lg-12 col-lg-offset-0">' + commentName + ' wrote: <br/> ' + commentText + '</div>');
-//     })
-// });
-
-// //Animation of main image
-// $(".mainimage").hover(function () {
-//     console.log('hovered');
-//     $('.mainimage').animate({
-//         width: '-=5px',
-//         height: '-=5px'
-//     }, 'slow');
-//     $('.mainimage').animate({
-//         width: '+=5px',
-//         height: '+=5px'
-//     }, 'slow');
-// });
